496a5edbdc4b188b7aec42c4755e4145fc919b6c
Vadim Zhukov <persgray@gmail.com>
2024-07-17 23:27:58 +0300

    measuring

--- func-args.c
+++ func-args.c
@@ -20,6 +20,7 @@ double f_copy(struct data input) {
 	res -= input.z;
 	return res;
 }
+double f_copy_ovr(struct data *input) { return f_copy(*input); }
 
 double f_ptr(struct data *input) {
 	double res;
@@ -33,6 +34,7 @@ double f_ptr(struct data *input) {
 	res -= input->z;
 	return res;
 }
+double f_ptr_ovr(struct data *input) { return f_ptr(input); }
 
 double f_sep(double a, double b, double c, double k, double w, double x, double y, double z) {
 	double res;
@@ -46,14 +48,28 @@ double f_sep(double a, double b, double c, double k, double w, double x, double 
 	res -= z;
 	return res;
 }
+double f_sep_ovr(struct data *input) { return f_sep(input->a, input->b, input->c, input->k, input->w, input->x, input->y, input->z); }
+
+void measure(struct data *d, double (*pf)(struct data *)) {
+	time_t start, end;
+	unsigned long long nops;
+
+	start = time(NULL);
+	time(&start);
+	for (nops = 0; nops < 100000000; nops++) {
+		pf(d);
+	}
+	end = time(NULL);
+	printf("it took %llu seconds\n", (unsigned long long)(end - start));
+}
 
 int
 main(int argc, char **argv) {
-	time_t start, end;
 	struct data d;
-	unsigned long long nops, step = 10000;
 
-	// TODO
+	measure(&d, f_copy_ovr);
+	measure(&d, f_ptr_ovr);
+	measure(&d, f_sep_ovr);
 
 	return 0;
 }
