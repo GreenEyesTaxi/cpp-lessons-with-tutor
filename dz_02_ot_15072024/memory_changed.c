241ceb94a395692fcd4f0a9d954d43353f6eaee8
Polina Vasileva <devchurka2000@gmail.com>
2024-07-20 00:52:42 +0300

    created new file with task, but it's working  wrong :(

---
+++ dz_02_ot_15072024/memory_changed.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	const size_t bufsz = 100;
+	char *buf = malloc(bufsz);
+	int ndigits = 0;
+
+    const char marks[] = {'.', ',', ':', ';', '?', '!', '(', ')', '[', ']'};
+    int nmarks = 0;
+
+	if (!fgets(buf, bufsz, stdin))
+		return 1;
+
+	for (char *p = buf; *p; p++) {//видимо, все еще не до конца понимаю эту строчку
+		if (*p >= '0' && *p <= '9')
+		    ndigits++;
+        else {// я не понимаю, как сюда подтянуть правильно те самые ASCII
+            for (int i = 0; i < sizeof(marks); i++) {
+                if (marks[i] == buf[*p]){
+                    nmarks++;  
+                }
+            }
+        }
+	}
+	printf("Found %d digits", ndigits);
+    printf(" and %d punctuation marks\n", nmarks);//не работает, но комплится 
+	return 0;
+}
