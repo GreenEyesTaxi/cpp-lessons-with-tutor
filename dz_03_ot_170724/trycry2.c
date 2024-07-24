b5abadd5b99f009f5aaa43cc8f20fe46a4d0e2d9
Polina Vasileva <devchurka2000@gmail.com>
2024-07-24 22:46:40 +0300

    ultra last vers

--- dz_03_ot_170724/trycry2.c
+++ dz_03_ot_170724/trycry2.c
@@ -15,32 +15,33 @@ test_data[] = {
     {1, 4e8, 1000, 2000} //тут должна быть ошибка по скорости света 
 };
 
-double calc_newton(double m, double v, double en)  {
+double calc_newton(double m, double v)  {
     /*if (m < 0.0000001 && m >= 0 || v > c) {
         return nan(""); 
     }*/
 
-    en = m * v * v / 2;
+    double en = m * v * v / 2;
     return en;
 
 }
 
-double calc_newton_ovr(struct data *input) {return calc_newton(input->m, input->v, input->en);}
+//double calc_newton_ovr(struct data *input) {return calc_newton(input->m, input->v);}
 
-double calc_einstein(double m, double v, double ee) {
+double calc_einstein(double m, double v) {
     
     /*if (m >= 0.0000001 && m==0 || v > c) {
         return nan("");
     }*/
 
-    ee = m * c * c / 1.6 / 1e-19;//ради приличия сделаю перевод в эвы
+    double ee = m * c * c;//ради приличия сделаю перевод в эвы
     return ee;
 }
 
-double calc_einstein_ovr(struct data *input) {return calc_einstein(input->m, input->v, input->ee);} 
+//double calc_einstein_ovr(struct data *input) {return calc_einstein(input->m, input->v);} 
 
-int verify_calc(struct data *d, double *actual, double (*calc)(struct data *)) {
-    return *actual == calc(d);
+int verify_calc(double m, double v, double expected, double *actual, double (*calc)(double m, double v)) {
+    *actual = calc(m,v);
+    return *actual == expected;
 }
 
 int main() {
@@ -50,7 +51,7 @@ int main() {
     int nerrs = 0;
 
     for (size_t i = 0; i < sizeof(test_data)/sizeof(test_data[0]); i++, test++) {
-        if (!verify_calc(test, &actual, calc_newton_ovr)) {
+        if (!verify_calc(test->m, test->v, test->en, &actual, calc_newton)) {
 			nerrs++;
 			printf("test failed for %f and %f: expected %f but got %f\n",
 			    test->m, test->v, test->en, actual);
@@ -58,7 +59,7 @@ int main() {
     }
 
     for (size_t i = 0; i < sizeof(test_data)/sizeof(test_data[0]); i++, test++) {    
-        if (!verify_calc(test, &actual, calc_einstein_ovr)) {
+        if (!verify_calc(test->m, test->v, test->en, &actual, calc_einstein)) {
             nerrs++;
             printf("test failed for %f and %f: expected %f but got %f\n",
 			    test->m, test->v, test->ee, actual);
