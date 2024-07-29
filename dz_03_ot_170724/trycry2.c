81105ad2fc034c6f9568a4f3ce693c1fec0f90f1
Polina Vasileva <devchurka2000@gmail.com>
2024-07-29 15:03:01 +0300

    version from 29.07.24

--- dz_03_ot_170724/trycry2.c
+++ dz_03_ot_170724/trycry2.c
@@ -5,14 +5,15 @@
 #include <float.h>
 
 const double c = 3e8;//скорость в  м/С
+double error_rate = 0.005;
 
 struct data {
     double m, v, en, ee;//масса в кг, скорость в м/с
 }
 test_data[] = {
-    //{1.6726e-27, 1, 938}, //протон, тут должен выдать ошибку, должно быть 938 000 000 или что-то по порядку
+    {1.6726e-27, 1, 938}, //протон, тут должен выдать ошибку, должно быть 938 000 000 или что-то по порядку
     {5, 10, 250, 4.5e17}, // должно пройти без ошибок
-    //{1, 4e8, 1000, 2000} //тут должна быть ошибка по скорости света 
+    {1, 4e8, 1000, NAN} //тут должна быть ошибка по скорости света 
 };
 
 double calc_newton(double m, double v)  {
@@ -26,14 +27,28 @@ double calc_einstein(double m, double v) {
         return nan("");
     }
 
-    double ee = m * c * c;//ради приличия сделаю перевод в эвы
+    double ee = m * c * c;
     return ee;
 }
 
 int verify_calc(double m, double v, double expected, double *actual, double (*calc)(double m, double v)) {
     *actual = calc(m,v);
-    return *actual == expected;
-}
+
+    if (isnan(expected)) {
+        return nan("");
+    }
+    
+    else {
+        if (*actual != expected) { 
+            return 0;
+        }
+
+        else if (fmod(*actual, expected) <= error_rate){
+            return 1;
+        }
+        
+    }
+}    
 
 int main() {
 
@@ -48,15 +63,12 @@ int main() {
 			printf("test failed for %f and %f: expected %f but got %f\n",
 			    test->m, test->v, test->en, actual);
 		}
-    }
-
-    for (size_t i = 0; i < sizeof(test_data)/sizeof(test_data[0]); i++, test++) {    
-        if (!verify_calc(test->m, test->v, test->en, &actual, calc_einstein)) {
+        if (!verify_calc(test->m, test->v, test->ee, &actual, calc_einstein)) {
             nerrs++;
             printf("test failed for %f and %f: expected %f but got %f\n",
 			    test->m, test->v, test->ee, actual);
         }
-	}
+    }
 
     if (nerrs != 0) {
         return 2;
