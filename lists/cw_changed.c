0037188b5b8ba3118aa415fb8c4629917703d7b0
Polina Vasileva <devchurka2000@gmail.com>
2024-08-01 16:28:48 +0300

    version with seg fault

--- lists/cw_changed.c
+++ lists/cw_changed.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct sample {
 	time_t timestamp;
@@ -33,10 +34,10 @@ int delete_item(struct sample *item_to_delete) {
 	return 1;
 }
 
-int insert_item_between(struct sample *item_to_insert, struct sample *previous, struct sample *following, time_t some_timestamp, double some_value) {
+int insert_item_between(struct sample *item_to_insert, struct sample *previous, struct sample *following, time_t *some_timestamp, double *some_value) {
     item_to_insert = malloc(sizeof(struct sample));
-	item_to_insert->timestamp = some_timestamp;
-	item_to_insert->value = some_value;
+	item_to_insert->timestamp = *some_timestamp;
+	item_to_insert->value = *some_value;
 
 	item_to_insert->next = following;
 	previous->next = item_to_insert;
@@ -51,21 +52,21 @@ int insert_item_between(struct sample *item_to_insert, struct sample *previous, 
 	}    
 } 
 
-int insert_item_back(struct sample *item_to_insert, time_t some_timestamp, double some_value) {
+int insert_item_back(struct sample *item_to_insert, time_t *some_timestamp, double *some_value) {
 	item_to_insert = malloc(sizeof(struct sample));
-	item_to_insert->timestamp = some_timestamp;
-	item_to_insert->value = some_value;
+	item_to_insert->timestamp = *some_timestamp;
+	item_to_insert->value = *some_value;
 
 	item_to_insert->next = NULL;
 
-	struct sample *prev = head; 
+	struct sample *current = head; 
 
-	while (prev != NULL) {
-		prev = prev->next;
+	while (current != NULL) {
+		current = current->next;
 	}
 		
-	if (prev == NULL) {
-		prev->next = item_to_insert;
+	if (current == NULL) {
+		current->next = item_to_insert;
 	}
 			
 	for (struct sample *compare = head; compare; compare = compare->next) {
@@ -93,7 +94,7 @@ int main(int argc, char **argv) {
 	s1->next = s2;
 	s2->next = NULL;
 
-	print_list(head);
+	//print_list(head);
 
 	// insert into list
 	struct sample *s12;
@@ -103,19 +104,19 @@ int main(int argc, char **argv) {
 
     scanf("%ti%lf", &timestamp_insert, &value_insert);
 	
-    insert_item_between(s12, s1, s2, timestamp_insert, value_insert);
-	insert_item_back(s12, timestamp_insert, value_insert);
-	insert_item_back(s3, timestamp_insert, value_insert);
+    insert_item_between(s12, s1, s2, &timestamp_insert, &value_insert);
+	insert_item_back(s12, &timestamp_insert, &value_insert);
+	insert_item_back(s3, &timestamp_insert, &value_insert);
 
 	print_list(head);
 
 	if (delete_item(s2) == 0)
 		fprintf(stderr, "delete(s2) failed\n");
-	print_list(head);
+	//print_list(head);
 
 	if (delete_item(s1) == 0)
 		fprintf(stderr, "delete(s1) failed\n");
-	print_list(head);
+	//print_list(head);
 
 	return 0;
 }
