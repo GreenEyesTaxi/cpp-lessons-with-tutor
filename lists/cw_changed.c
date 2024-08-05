8f431555142be111f5c070bbffbbd86170be5137
Polina Vasileva <devchurka2000@gmail.com>
2024-08-05 16:02:02 +0300

    working version 05.08.24 all inserts in one function

--- lists/cw_changed.c
+++ lists/cw_changed.c
@@ -34,52 +34,43 @@ int delete_item(struct sample *item_to_delete) {
 	return 1;
 }
 
-int insert_item_between(struct sample *item_to_insert, struct sample *previous, struct sample *following, time_t *some_timestamp, double *some_value) {
-    item_to_insert = malloc(sizeof(struct sample));
-	item_to_insert->timestamp = *some_timestamp;
-	item_to_insert->value = *some_value;
-
-	item_to_insert->next = following;
-	previous->next = item_to_insert;
+int insert_item_between(struct sample *item_to_insert, struct sample *previous, time_t some_timestamp, double some_value) {
+	item_to_insert->timestamp = some_timestamp;
+	item_to_insert->value = some_value;
 	
 	for (struct sample *compare = head; compare; compare = compare->next) {
 		if (compare == item_to_insert) {
         	return 0;//fail
     	}
-    	else {
-        	return 1;//success
-    	}
-	}    
+	}
+
+	if (previous == NULL) {//вставка в начало 
+		item_to_insert->next = head;
+		head = item_to_insert;
+	} else {
+		if (previous->next == NULL) {//вставка в конец
+			struct sample *current = head; 
+
+			if (current == NULL) {
+				current->next = item_to_insert;
+			}
+			else {
+				while (current->next != NULL) {
+				current = current->next;
+				}
+
+				current->next = item_to_insert;
+				item_to_insert->next = NULL;
+			}
+		}
+		else {//вставка в середину 
+			item_to_insert->next = previous->next;
+			previous->next = item_to_insert;
+		}
+	}
+
+	return 1;//success    
 } 
-
-int insert_item_back(struct sample *item_to_insert, time_t *some_timestamp, double *some_value) {
-	item_to_insert = malloc(sizeof(struct sample));
-	item_to_insert->timestamp = *some_timestamp;
-	item_to_insert->value = *some_value;
-
-	item_to_insert->next = NULL;
-
-	struct sample *current = head; 
-
-	while (current != NULL) {
-		current = current->next;
-	}
-		
-	if (current == NULL) {
-		current->next = item_to_insert;
-	}
-			
-	for (struct sample *compare = head; compare; compare = compare->next) {
-		if (compare == item_to_insert) {
-        	return 0;//fail
-    	}
-    	else {
-        	return 1;//success
-    	}
-	}
-}
-
-
 
 int main(int argc, char **argv) {
 	struct sample *s1 = malloc(sizeof(struct sample));
@@ -94,29 +85,36 @@ int main(int argc, char **argv) {
 	s1->next = s2;
 	s2->next = NULL;
 
-	//print_list(head);
-
 	// insert into list
-	struct sample *s12;
-	struct sample *s3;
-    time_t timestamp_insert;
-    double value_insert;
+	struct sample *s12  = malloc(sizeof(struct sample));
+	time_t timestamp_insert12;
+    double value_insert12;
+    scanf("%ti%lf", &timestamp_insert12, &value_insert12);
 
-    scanf("%ti%lf", &timestamp_insert, &value_insert);
+	struct sample *s3 = malloc(sizeof(struct sample));
+	time_t timestamp_insert3;
+    double value_insert3;
+    scanf("%ti%lf", &timestamp_insert3, &value_insert3);
+
+    struct sample *s4 = malloc(sizeof(struct sample));
+	time_t timestamp_insert4;
+    double value_insert4;
+    scanf("%ti%lf", &timestamp_insert4, &value_insert4);
 	
-    insert_item_between(s12, s1, s2, &timestamp_insert, &value_insert);
-	insert_item_back(s12, &timestamp_insert, &value_insert);
-	insert_item_back(s3, &timestamp_insert, &value_insert);
+    insert_item_between(s12, s1, timestamp_insert12, value_insert12); //между s1 и s2
+	insert_item_between(s3, s2, timestamp_insert3, value_insert3);//в конец
+	insert_item_between(s4, NULL, timestamp_insert4, value_insert4);//в начало 
 
 	print_list(head);
-
-	if (delete_item(s2) == 0)
+	
+	delete_item(head);
+	/*if (delete_item(s2) == 0)
 		fprintf(stderr, "delete(s2) failed\n");
 	//print_list(head);
 
 	if (delete_item(s1) == 0)
 		fprintf(stderr, "delete(s1) failed\n");
-	//print_list(head);
+	//print_list(head);*/
 
 	return 0;
 }
